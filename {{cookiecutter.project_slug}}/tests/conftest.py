c334dc327b82454dd41faafa47f02b33937e9a04
Pedro Crespo <crespo@itis.ethz.ch>
2018-10-08 16:23:21 +0200

    Minor fixes and convenient extra info

--- {{cookiecutter.project_slug}}/tests/conftest.py
+++ {{cookiecutter.project_slug}}/tests/conftest.py
@@ -2,6 +2,14 @@
 # pylint: disable=W0611
 # TODO: W0613:Unused argument ...
 # pylint: disable=W0613
+import sys
 
 import pytest
+
+from pathlib import Path
+
+
+@pytest.fixture
+def here():
+    return Path(sys.argv[0] if __name__ == "__main__" else __file__).resolve().parent
 
--- {{cookiecutter.project_slug}}/tests/test_{{cookiecutter.package_name}}.py
+++ {{cookiecutter.project_slug}}/tests/test_{{cookiecutter.package_name}}.py
@@ -2,15 +2,10 @@
 # pylint: disable=W0611
 # TODO: W0613:Unused argument ...
 # pylint: disable=W0613
-from pathlib import Path
+
+import pytest
 
 from {{ cookiecutter.package_name }}.cli import main
 
-
-@pytest.fixture
-def here():
-    return Path(sys.argv[0] if __name__ == "__main__" else __file__).resolve().parent
-
-
-def test_main():
-    main([])
+def test_main(here): # pylint: disable=unused-variable    
+    main("--help".split())
