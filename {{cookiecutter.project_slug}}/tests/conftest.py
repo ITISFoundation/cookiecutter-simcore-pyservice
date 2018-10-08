4ab0efa0d55f9655a7a6a0dc970501e4866c496b
Pedro Crespo <crespo@itis.ethz.ch>
2018-10-08 15:49:08 +0200

    Installs rope for code substitution/renaming (integrated in vscode) Disable pylint unimportant warnings in tests Add handy fixture to get current's directory

---
+++ {{cookiecutter.project_slug}}/tests/conftest.py
@@ -0,0 +1,7 @@
+# TODO: W0611:Unused import ...
+# pylint: disable=W0611
+# TODO: W0613:Unused argument ...
+# pylint: disable=W0613
+
+import pytest
+
--- {{cookiecutter.project_slug}}/tests/test_{{cookiecutter.package_name}}.py
+++ {{cookiecutter.project_slug}}/tests/test_{{cookiecutter.package_name}}.py
@@ -1,4 +1,15 @@
+# TODO: W0611:Unused import ...
+# pylint: disable=W0611
+# TODO: W0613:Unused argument ...
+# pylint: disable=W0613
+from pathlib import Path
+
 from {{ cookiecutter.package_name }}.cli import main
+
+
+@pytest.fixture
+def here():
+    return Path(sys.argv[0] if __name__ == "__main__" else __file__).resolve().parent
 
 
 def test_main():
