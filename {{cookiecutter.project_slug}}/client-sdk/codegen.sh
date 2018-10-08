efcd3c0f67196661b67a2f0db10e1683f4fa5dc4
Pedro Crespo <crespo@itis.ethz.ch>
2018-10-08 17:18:16 +0200

    Fixes for client sdk config

--- {{cookiecutter.project_slug}}/client-sdk/codegen.sh
+++ {{cookiecutter.project_slug}}/client-sdk/codegen.sh
@@ -1,7 +1,7 @@
 #/bin/bash
 # TODO: unify scripts
 exec {{ cookiecutter.simcore_install_root }}/scripts/openapi/openapi_codegen.sh \
-    -i {{ cookiecutter.simcore_install_root }}/services/{{ cookiecutter.project_slug }}/src/{{ cookiecutter.package_name }}/oas3/{{cookiecutter.openapi_specs_version}}/openapi.yaml \
+    -i ../src/{{ cookiecutter.package_name }}/oas3/{{cookiecutter.openapi_specs_version}}/openapi.yaml \
     -o . \
     -g python \
     -c ./codegen_config.json
