1c7a8b3043ec8279b365c413142c4c4e46b2da62
Pedro Crespo <crespo@itis.ethz.ch>
2018-10-09 17:44:39 +0200

    Replaces flake8 by pylint test. cookiecutter tests now pass.

--- tests/test_bake_project.py
+++ tests/test_bake_project.py
@@ -1,7 +1,24 @@
+#pylint: disable=W0621 
+# W0621:Redefining name 'here' from outer scope (line 12)
+import os
+import sys
+import pytest
+import subprocess
 from contextlib import contextmanager
 
-import os
-import subprocess
+from pathlib import Path
+
+
+
+@pytest.fixture
+def here():
+    return Path(sys.argv[0] if __name__ == "__main__" else __file__).resolve().parent
+
+@pytest.fixture
+def pylintrc(here):
+    path = here.parent / ".pylintrc"
+    assert path.exists()
+    return path
 
 
 @contextmanager
@@ -23,9 +40,16 @@ def test_project_tree(cookies):
     assert result.exit_code == 0
     assert result.exception is None
     assert result.project.basename == 'test_project'
+    assert result.project.isdir()
 
+# TODO: activate flake8 together with pylint. Need a config file first...
+#def test_run_flake8(cookies):
+#    result = cookies.bake(extra_context={'project_slug': 'flake8_compat'})
+#    with inside_dir(str(result.project)):
+#        assert subprocess.check_call(['flake8']) == 0
 
-def test_run_flake8(cookies):
-    result = cookies.bake(extra_context={'project_slug': 'flake8_compat'})
+def test_run_pylint(cookies, pylintrc):
+    result = cookies.bake(extra_context={'project_slug': 'pylint_compat', 'package_name': 'package_folder'})    
     with inside_dir(str(result.project)):
-        assert subprocess.check_call(['flake8']) == 0
+        cmd = 'pylint --rcfile {} -v src/package_folder/'.format(pylintrc.absolute()).split()
+        assert subprocess.check_call(cmd) == 0
