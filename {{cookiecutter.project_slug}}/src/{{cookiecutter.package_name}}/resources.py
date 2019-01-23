ee68772b0aa7f5880b3fbf7cb488b1f5352ff28a
Pedro Crespo <crespo@itis.ethz.ch>
2019-01-23 17:57:44 +0100

    Simplified resources package

--- {{cookiecutter.project_slug}}/src/{{cookiecutter.package_name}}/resources.py
+++ {{cookiecutter.project_slug}}/src/{{cookiecutter.package_name}}/resources.py
@@ -1,25 +1,16 @@
-""" Access to data resources installed with this package
+""" Access to data resources installed within this package
+
 
 """
-from pathlib import Path
-from simcore_servicelib.resources import Resources
+from servicelib.resources import ResourcesFacade
 
-from .settings import RSC_CONFIG_KEY, RSC_OPENAPI_KEY #pylint: disable=unused-import
-from .settings import OAS_ROOT_FILE
-
-resources = Resources(__name__, config_folder='etc/simcore_service_storage')
-
-
-def openapi_path() -> Path:
-    """ Returns path to the roots's oas file
-    Notice that the specs can be split in multiple files. Thisone
-    is the root file and it is normally named as `opeapi.yaml`
-    """
-    return resources.get_path(OAS_ROOT_FILE)
+resources = ResourcesFacade(
+    package_name=__name__,
+    distribution_name="{{ cookiecutter.distribution_name }}",
+    config_folder='config',
+)
 
 
 __all__ = (
     'resources',
-    'RSC_CONFIG_KEY',
-    'RSC_OPENAPI_KEY'
 )
