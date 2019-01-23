b745070de72ef8f313d88eb00af875bb362359e2
Pedro Crespo <crespo@itis.ethz.ch>
2019-01-23 19:58:15 +0100

    Updated cli  - separated parts  - updated tests (renamed test_package.py)

--- {{cookiecutter.project_slug}}/src/{{cookiecutter.package_name}}/resources.py
+++ {{cookiecutter.project_slug}}/src/{{cookiecutter.package_name}}/resources.py
@@ -1,8 +1,13 @@
 """ Access to data resources installed within this package
 
-
 """
+from typing import Dict
+
+import pkg_resources
+
 from servicelib.resources import ResourcesFacade
+
+_META_PKG_INFO = 'PKG-INFO'
 
 resources = ResourcesFacade(
     package_name=__name__,
@@ -10,7 +15,14 @@ resources = ResourcesFacade(
     config_folder='config',
 )
 
+def get_distribution_info() -> Dict:
+    """ Returns distributon information, as  provided in setup """
+    dist = pkg_resources.get_distribution('{{ cookiecutter.distribution_name }}')
+    info = dist.get_metadata(_META_PKG_INFO) if dist.has_metadata(_META_PKG_INFO) else {}
+    return info
+
 
 __all__ = (
     'resources',
+    'get_distribution_info'
 )
--- {{cookiecutter.project_slug}}/tests/conftest.py
+++ {{cookiecutter.project_slug}}/tests/conftest.py
@@ -5,13 +5,20 @@
 # pylint:disable=redefined-outer-name
 
 import sys
+from pathlib import Path
 
 import pytest
 
-from pathlib import Path
+from servicelib.utils import search_osparc_repo_dir
 
 
 @pytest.fixture
 def here():
     return Path(sys.argv[0] if __name__ == "__main__" else __file__).resolve().parent
 
+@pytest.fixture(scope='session')
+def osparc_simcore_root_dir():
+    root_dir = search_osparc_repo_dir()
+    assert root_dir and root_dir.exists(), "Is this service within osparc-simcore repo?"
+    assert any(root_dir.glob("services/{{ cookiecutter.project_slug }}")), "%s not look like rootdir" % root_dir
+    return root_dir
