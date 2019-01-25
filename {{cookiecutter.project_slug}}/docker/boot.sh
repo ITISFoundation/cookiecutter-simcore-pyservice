34e51d6ae6ddbf769081c1039541e5eeef421b53
Pedro Crespo <crespo@itis.ethz.ch>
2019-01-25 11:57:48 +0100

    Updates Dockerfile Enables pdb debugging with DEBUG=2

--- {{cookiecutter.project_slug}}/docker/boot.sh
+++ {{cookiecutter.project_slug}}/docker/boot.sh
@@ -9,12 +9,20 @@ then
   echo "DEBUG: Workdir :`pwd`"
 
   cd $HOME/services/{{ cookiecutter.project_slug }}
-  pip install -r requirements/dev.txt
+  pip install -r requirements/base.txt
+  pip install -r requirements/tests.txt
+  pip install -e .
   pip list
 
-  cd $HOME/  
-  {{ cookiecutter.command_line_interface_bin_name }} --config config-dev.yaml
+  cd $HOME/
+  {{ cookiecutter.command_line_interface_bin_name }} --config config-host-dev.yaml
+
+elif [[ ${DEBUG} == "2" ]]
+then
+  echo "Booting with debugger attached: https://docs.python.org/3.6/library/pdb.html#debugger-commands  ..."
+  python -c "import pdb, {{ cookiecutter.package_name }}.cli; pdb.run('{{ cookiecutter.package_name }}.cli.main([\'-c\',\'config-container-prod.yaml\'])')"
+
 else
   echo "Booting in production mode ..."
-  {{ cookiecutter.command_line_interface_bin_name }} --config config-prod.yaml
+  {{ cookiecutter.command_line_interface_bin_name }} --config config-container-prod.yaml
 fi
