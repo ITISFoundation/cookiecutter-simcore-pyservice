37b59df6d26c78c00e65473c0c62366f9f6f501a
Pedro Crespo <crespo@itis.ethz.ch>
2019-01-25 15:35:16 +0100

    Renamed docker environemtn variable DEBUG->BOOT_MODE Updated requirement file names

--- {{cookiecutter.project_slug}}/docker/boot.sh
+++ {{cookiecutter.project_slug}}/docker/boot.sh
@@ -1,28 +1,32 @@
 #!/bin/sh
+#
 echo "Activating python virtual env..."
 source $HOME/.venv/bin/activate
 
-if [[ ${DEBUG} == "1" ]]
+echo "Booting {{ cookiecutter.project_slug }} in ${BOOT_MODE} mode ..."
+
+if [[ ${BOOT_MODE} == "development" ]]
 then
-  echo "Booting in development mode ..."
-  echo "DEBUG: User    :`id $(whoami)`"
-  echo "DEBUG: Workdir :`pwd`"
+  echo "INFO: User    :`id $(whoami)`"
+  echo "INFO: Workdir :`pwd`"
 
   cd $HOME/services/{{ cookiecutter.project_slug }}
-  pip install -r requirements/base.txt
-  pip install -r requirements/tests.txt
+  pip install -r requirements/ci.txt
   pip install -e .
   pip list
 
   cd $HOME/
   {{ cookiecutter.command_line_interface_bin_name }} --config config-host-dev.yaml
 
-elif [[ ${DEBUG} == "2" ]]
+elif [[ ${BOOT_MODE} == "debug" ]]
 then
-  echo "Booting with debugger attached: https://docs.python.org/3.6/library/pdb.html#debugger-commands  ..."
+  echo "INFO: Debugger attached: https://docs.python.org/3.6/library/pdb.html#debugger-commands  ..."
   python -c "import pdb, {{ cookiecutter.package_name }}.cli; pdb.run('{{ cookiecutter.package_name }}.cli.main([\'-c\',\'config-container-prod.yaml\'])')"
 
-else
-  echo "Booting in production mode ..."
+elif [[ ${BOOT_MODE} == "production" ]]
+then
   {{ cookiecutter.command_line_interface_bin_name }} --config config-container-prod.yaml
+
+else
+  echo "ERROR: ${BOOT_MODE} is invalid booting mode. Expecting development, debug or production"
 fi
