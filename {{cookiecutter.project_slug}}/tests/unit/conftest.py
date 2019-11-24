0badc493fa17c7801d3df14d7be2c09283a350e6
Pedro Crespo <crespo@itis.ethz.ch>
2019-11-24 04:07:45 +0100

    Updated unit tests

--- {{cookiecutter.project_slug}}/tests/unit/conftest.py
+++ {{cookiecutter.project_slug}}/tests/unit/conftest.py
@@ -1,5 +1,3 @@
-# pylint:disable=wildcard-import
-# pylint:disable=unused-import
 # pylint:disable=unused-variable
 # pylint:disable=unused-argument
 # pylint:disable=redefined-outer-name
@@ -14,28 +12,26 @@ from servicelib.utils import search_osparc_repo_dir
 import {{ cookiecutter.package_name }}
 
 
-@pytest.fixture(scope='session')
-def here():
-    return Path(sys.argv[0] if __name__ == "__main__" else __file__).resolve().parent
+current_dir = Path(sys.argv[0] if __name__ == "__main__" else __file__).resolve().parent
 
 
 @pytest.fixture(scope='session')
-def project_slug_dir(here):
-    folder = here.parent.parent
+def project_slug_dir():
+    folder = current_dir.parent.parent
     assert folder.exists()
     assert any( folder.glob("src/{{ cookiecutter.package_name }}") )
     return folder
 
 @pytest.fixture(scope='session')
-def package_dir(here):
+def package_dir():
     dirpath = Path({{ cookiecutter.package_name }}.__file__).resolve().parent
     assert dirpath.exists()
     return dirpath
 
 
 @pytest.fixture(scope='session')
-def osparc_simcore_root_dir(here):
-    root_dir = search_osparc_repo_dir(here) or here.parent.parent / "_osparc-simcore-stub"
+def osparc_simcore_root_dir():
+    root_dir = search_osparc_repo_dir(current_dir) or here.parent.parent / "_osparc-simcore-stub"
     assert root_dir and root_dir.exists(), "Did you renamed or moved the integration folder under {{ cookiecutter.project_slug }}??"
     assert any(root_dir.glob("services/{{ cookiecutter.project_slug }}")), "%s not look like rootdir" % root_dir
     return root_dir
