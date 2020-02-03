117f234c71c7fab922265b97769470dab69472a2
Pedro Crespo <crespo@itis.ethz.ch>
2020-02-03 11:40:34 +0100

    Simplified version

--- {{cookiecutter.project_slug}}/setup.py
+++ {{cookiecutter.project_slug}}/setup.py
@@ -9,25 +9,26 @@ from setuptools import find_packages, setup
 if sys.version_info.major != 3 and sys.version_info.minor != 6:
     raise RuntimeError("Expected ~=3.6, got %s. Did you forget to activate virtualenv?" % str(sys.version_info))
 
-here = Path(sys.argv[0] if __name__ == "__main__" else __file__).resolve().parent
+current_dir = Path(sys.argv[0] if __name__ == "__main__" else __file__).resolve().parent
 
 
 def read_reqs( reqs_path: Path):
     return re.findall(r'(^[^#-][\w]+[-~>=<.\w]+)', reqs_path.read_text(), re.MULTILINE)
 
 
-readme = (here/'README.md').read_text()
+readme = (current_dir/'README.md').read_text()
+version = (current_dir/"VERSION").read_text().strip()
 
-install_requirements = read_reqs( here / "requirements" / "base.in" ) + [
+install_requirements = read_reqs( current_dir / "requirements" / "base.in" ) + [
     'simcore-service-library~=0.1'
 ]
 
-test_requirements = read_reqs( here / "requirements" / "test.in" )
+test_requirements = read_reqs( current_dir / "requirements" / "test.in" )
 
 
 setup(
     name="{{ cookiecutter.distribution_name }}",
-    version="{{ cookiecutter.version }}",
+    version=version,
     author="{{ '{full_name} ({github_username})'.format(**cookiecutter) }}",
     description={{ '{0!r}'.format(cookiecutter.project_short_description).lstrip('ub') }},
     classifiers=[
@@ -39,7 +40,7 @@ setup(
     long_description=readme,
     license="MIT license",
     python_requires='~=3.6',
-    packages=find_packages(where='src'),
+    packages=find_packages(wcurrent_dir='src'),
     package_dir={
         '': 'src',
     },
