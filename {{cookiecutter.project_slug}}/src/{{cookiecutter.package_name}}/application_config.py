990c1da251c4a7113254ba9abfd25b324c7173d9
Andrei Neagu <neagu@itis.swiss>
2020-05-14 15:44:34 +0200

    added extra check to be disabled

--- {{cookiecutter.project_slug}}/src/{{cookiecutter.package_name}}/application_config.py
+++ {{cookiecutter.project_slug}}/src/{{cookiecutter.package_name}}/application_config.py
@@ -53,8 +53,7 @@ def create_schema():
 
 
  # app[APP_CONFIG_KEY] = key for config object
- # pylint: disable=self-assigning-variable
-APP_CONFIG_KEY = APP_CONFIG_KEY
+APP_CONFIG_KEY = APP_CONFIG_KEY # pylint: disable=self-assigning-variable, bad-option-value
 
 # config/${CLI_DEFAULT_CONFIGFILE}
 CLI_DEFAULT_CONFIGFILE = 'config-container-prod.yml'
